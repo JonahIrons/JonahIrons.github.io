88aa6a884667f1544b14642f0bcdd112aafcbdc6
Jonah Irons <143219746+JonahIrons@users.noreply.github.com>
2024-12-27 16:52:17 -0500

    Implemented image carousel to header

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-/*const images = ["images/carousel/1st.jpeg", "images/carousel/irons-boys.jpeg", "images/carousel/jonah-turkstra.jpeg", "images/carousel/jonah-zane-ben.jpeg", "images/carousel/jonah-parents.jpeg"];
+/* CARAOUSEL */
+const images = ["images/carousel/jonah-turkstra.jpeg", "images/carousel/irons-boys.jpeg", "images/carousel/jonah-zane-ben.jpeg", "images/carousel/jonah-parents.jpeg"];
 const carousel = document.querySelector(".carousel");
 const interval = setInterval(function() {
     startCarousel();
-}, 4000); /* How often the carousel changes images
+}, 6000); /* How often the carousel changes images */
 var index = 1;
 
 startCarousel = () => {
@@ -11,20 +12,23 @@ startCarousel = () => {
     void carousel.offsetWidth;
     carousel.classList.add("fade");
     if(index > images.length - 1) index = 0;
-} */
+}
 
-    var tablinks = document.getElementsByClassName("tab-links");
-    var tabcontents = document.getElementsByClassName("tab-contents");
 
-    function opentab(tabname) {
-        for (tablink of tablinks) {
-            tablink.classList.remove("active-link");
-        }
 
-        for (tabcontent of tabcontents) {
-            tabcontent.classList.remove("active-tab");
-        }
+/* ABOUT SECTION TAB FUNCTIONALITY */
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
 
-        event.currentTarget.classList.add("active-link");
-        document.getElementById(tabname).classList.add("active-tab");
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
     }
+
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
